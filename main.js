31765c3224e1beb3f1e04c5c51fb3cf2aa2fa5af
Joe Finny <finne132@umn.edu>
2018-03-20 18:59:41 -0500

    another comment

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 "use strict";
 
 // 1. Build a query string
+//      form will have: search term entry, number of articles to get, start year for article(optional), end year for article (optional)
 // 2. make the API call, format and display the response 
 // 3. clear the results 
 
 // listeners: 
 // 1. onClick for Search button 
 // 2. onClick for Clear button
+
