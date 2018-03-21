aa3b7c982eaf07405743c7aafa307277926f30ca
Joe Finny <finne132@umn.edu>
2018-03-20 19:11:20 -0500

    added basic starting variables

--- main.js
+++ main.js
@@ -9,5 +9,11 @@
 // 1. onClick for Search button 
 // 2. onClick for Clear button
 
-let queryBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=c0472e9778e24819951ef447805c93c1"
+let queryBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=c0472e9778e24819951ef447805c93c1&q=";
+let search = $('#TEXT INPUT BOX ID').val();
+let numArticles = 0;
+let startYear = 0; 
+let endYear = 0;
+
+
 
